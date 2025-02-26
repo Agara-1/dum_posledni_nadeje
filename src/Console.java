85cc3398d5681f638e674849e0baac02602850fb
Michal Sakal <sakalmichal50@gmail.com>
2025-02-26 22:36:10 +0100

    v konzoli zpracovavame prikazy od hrace

---
+++ src/Console.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Console {
+    private boolean exit = false;
+    private Scanner scanner = new Scanner(System.in);
+    private HashMap<String, Command> prikazy = new HashMap<>();
+    private SvetovaMapa sm = new SvetovaMapa();
+
+public void inicializace(){
+    prikazy.put("jdi", new Pohyb(new Mistnost("Chodba")));
+}
+private Scanner sc = new Scanner(System.in);
+
+public void delejPrikaz() throws IOException {
+    System.out.println(sm.getSvet());
+    System.out.println(">>");
+    String command = sc.nextLine();
+    command = command.toLowerCase();
+    command = command.trim();
+    if(prikazy.containsKey(command)){
+        System.out.println( prikazy.get(command).execute());
+
+    }else {
+        System.out.println("Neznamy prikaz");
+    }
+}
+
+public void start() throws IOException {
+    inicializace();
+    do{
+        delejPrikaz();
+    }while(!exit);
+
+}
+
+}
