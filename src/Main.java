1caa6ad175ba56c79f8a83f1a02ece167a868468
Michal Sakal <sakalmichal50@gmail.com>
2025-02-26 22:33:35 +0100

    v metode main se spousti program

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+Console c = new Console();
 
+        try {
+            c.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     }
