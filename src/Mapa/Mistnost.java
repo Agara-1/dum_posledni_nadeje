e468c145bab62c8a0e6af59aab339bac35d72fc2
Michal Sakal <sakalmichal50@gmail.com>
2025-03-19 22:35:37 +0100

    nove nacitani mapy kvuli pridavani predmetu do pokoju a pak jejich sebrani

--- src/Mapa/Mistnost.java
+++ src/Mapa/Mistnost.java
@@ -1,67 +1,79 @@
 package Mapa;
 
+import Postavy.Hrac;
 import Postavy.Postava;
+import Postavy.Vezen;
+import Veci.Akumulatory;
 import Veci.Predmet;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 
-public class Mistnost {
-   private String nazev;
 
-private ArrayList<Predmet> predmetyVMistnosti;
-private ArrayList<Postava> postavyVMistnosti;
 
-   public Mistnost() {
+   public class Mistnost {
+      private String nazev;
+      private ArrayList<Postava> postavyVMistnosti;
+      private Predmet predmetyVMistnosti;
+      public Mistnost(String nazev) {
+         this.nazev = nazev;
+         this.postavyVMistnosti = new ArrayList<>();
+
+      }
+
+      public String getNazev() {
+         return nazev;
+      }
+
+      public void pridaniPostavy(Postava p) {
+         postavyVMistnosti.add(p);
+      }
+
+      public ArrayList<Postava> getPostavyVMistnosti() {
+         return postavyVMistnosti;
+      }
+
+      public void pridatPredmet(Predmet p) {
+         this.predmetyVMistnosti = p;
+      }
+
+      public void odstranitPredmet(Predmet p) {
+         this.predmetyVMistnosti = null;
+      }
+
+      public Predmet getPredmetyVMistnosti() {
+         return predmetyVMistnosti;
+      }
+
+
+      Postava vezen = null;
+      public void vstoupit(Hrac hrac) {
+         Mistnost mojePozice = hrac.getMojePozice();
+         if ((mojePozice.getNazev().equals("Obyvak") || mojePozice.getNazev().equals("Chodba")) && Akumulatory.radioZapnuto) {
+                  System.out.println("\nJakmile vstoupíš do chodby, cítíš něco divného...\n" +
+                          "Na podlaze v obyvaku leží vězeň, jeho tělo nehybné, oči prázdné.\n" +
+                          "Ostatní vypadají šokovaně, ale nejvíce tě znepokojuje výraz psychologa.\n" +
+                          "Pomalu si přisedne vedle těla a zamyšleně řekne:\n" +
+                          "\"Ne všichni si zaslouží přežít... To byla nutná oběť.\"\n");
+         }
+      }
+
+      @Override
+      public String toString() {
+         return nazev;
+      }
+
+      @Override
+      public boolean equals(Object o) {
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         Mistnost mistnost = (Mistnost) o;
+         return Objects.equals(nazev, mistnost.nazev);
+      }
+
+      @Override
+      public int hashCode() {
+         return Objects.hashCode(nazev);
+      }
+
    }
-
-   public Mistnost(String nazev) {
-      this.nazev = nazev;
-      this.postavyVMistnosti = new ArrayList<>();
-      this.predmetyVMistnosti = new ArrayList<>();
-   }
-
-   
-   public String getNazev() {
-      return nazev;
-   }
-   public void pridaniPostavy(Postava po) {
-      postavyVMistnosti.add(po);
-
-   }
-public void pridatPredmet(Predmet p){
-      predmetyVMistnosti.add(p);
-   }
-public void odstranitPredmetZMistnosti(Predmet p){
-      predmetyVMistnosti.remove(p);
-}
-
-   public ArrayList<Predmet> getPredmetyVMistnosti() {
-      return predmetyVMistnosti;
-   }
-
-   public ArrayList<Postava> getPostavyVMistnosti() {
-      return postavyVMistnosti;
-   }
-
-   @Override
-   public String toString() {
-      return
-                nazev + '\''
-              ;
-   }
-
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      Mistnost mistnost = (Mistnost) o;
-      return Objects.equals(nazev, mistnost.nazev);
-   }
-
-   @Override
-   public int hashCode() {
-      return Objects.hashCode(nazev);
-   }
-}
 
--- src/Mapa/SvetovaMapa.java
+++ src/Mapa/SvetovaMapa.java
@@ -6,56 +6,40 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class SvetovaMapa {
-private HashMap<Mistnost, ArrayList<Mistnost>> svet = new HashMap<>();
-
+    private HashMap<String, Mistnost> mistnosti = new HashMap<>();
+    private HashMap<Mistnost, ArrayList<Mistnost>> svet = new HashMap<>();
 
     public SvetovaMapa() {
-        nactitaniMapy();
+        nactiMapu();
     }
 
-    public boolean nactitaniMapy(){
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("mapa.csv"));
+    private void nactiMapu() {
+        try (BufferedReader br = new BufferedReader(new FileReader("mapa.csv"))) {
             String line;
-
-
-            while((line = br.readLine())  != null ){
-
-                String[] rozdelene = line.split(",");
-                Mistnost m = new Mistnost(rozdelene[0]);
-                ArrayList<Mistnost> sousedniMistnosti = new ArrayList<>();
-
-                for ( String nazevMistnosti: Arrays.copyOfRange(rozdelene, 1, rozdelene.length)) {
-                    sousedniMistnosti.add(new Mistnost(nazevMistnosti));
+            while ((line = br.readLine()) != null) {
+                String[] casti = line.split(",");
+                Mistnost mistnost = mistnosti.computeIfAbsent(casti[0].trim(), Mistnost::new);
+                ArrayList<Mistnost> sousedi = svet.computeIfAbsent(mistnost, k -> new ArrayList<>());
+                for (int i = 1; i < casti.length; i++) {
+                    sousedi.add(mistnosti.computeIfAbsent(casti[i].trim(), Mistnost::new));
                 }
-
-                svet.put(m, sousedniMistnosti);
             }
-
         } catch (IOException e) {
             e.printStackTrace();
-
         }
-
-return true;
     }
 
-    public Mistnost getMistnost(String nazevMistnosti) {
-        for (Mistnost m : svet.keySet()) {
-            if (m.getNazev().equalsIgnoreCase(nazevMistnosti)) {
-                return m;
-            }
-        }
-        return null;
+    public Mistnost getMistnost(String nazev) {
+        return mistnosti.get(nazev);
     }
 
-
-    public ArrayList<Mistnost> sousedniMistnost(Mistnost m){
+    public ArrayList<Mistnost> sousedniMistnost(Mistnost m) {
         return svet.getOrDefault(m, new ArrayList<>());
     }
 
     public HashMap<Mistnost, ArrayList<Mistnost>> getSvet() {
         return svet;
     }
-
 }
+
+
